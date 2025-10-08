30cb994113d4cbdbb40af5a1b9250ba3c0577b17
Anhpqdhe172622 <anhpqdhe172622@fpt.edu.vn>
2025-10-08 22:16:03 +0700

    add Module
    add RepositoryModule and ServiceModule

---
+++ NB.API/Modules/RepositoryModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using System.Reflection;
+
+namespace NB.API.Modules
+{
+    public class RepositoryModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(Assembly.Load("NB.Repository"))
+                   .Where(t => t.Name.EndsWith("Repository"))
+                   .AsImplementedInterfaces()
+                  .InstancePerRequest();
+        }
+    }
+}
---
+++ NB.API/Modules/ServiceModule.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using System.Reflection;
+
+namespace NB.API.Modules
+{
+    public class ServiceModule : Autofac.Module
+    {
+
+        protected override void Load(ContainerBuilder builder)
+        {
+
+            builder.RegisterAssemblyTypes(Assembly.Load("NB.Service"))
+
+                      .Where(t => t.Name.EndsWith("Service"))
+
+                      .AsImplementedInterfaces()
+
+                      .InstancePerRequest();
+
+        }
+
+    }
+}
