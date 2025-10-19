281612cbafb1d66d4298b5b3a557cd3e1c0c8a68
trungpqhe173313 <trungpqhe173313@fpt.edu.vn>
2025-10-20 01:17:38 +0700

    Fix  codebase

--- NB.API/Program.cs
+++ NB.API/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using NB.API.Modules;
 using NB.Model.Entities;
@@ -6,27 +7,19 @@ using NB.Repository.Common;
 using NB.Service.EmployeeService;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//builder.Services.AddDbContext<NutriBarnContext>(options =>
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Đăng ký service
-builder.Services.AddScoped<IEmployeeService, EmployeeService>();
-builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-
-//builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
-//{
-//    //containerBuilder.RegisterModule<EFModule>();
-//    containerBuilder.RegisterModule<RepositoryModule>();
-//    containerBuilder.RegisterModule<ServiceModule>();
-//});
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
+{
+    //containerBuilder.RegisterModule<EFModule>();
+    containerBuilder.RegisterModule<RepositoryModule>();
+    containerBuilder.RegisterModule<ServiceModule>();
+});
 
 
 builder.Services.AddDbContext<NutriBarnContext>(options =>
