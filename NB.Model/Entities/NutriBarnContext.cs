8404645a9250d4a897954b296a5223269261d92b
QuanHTHE181258 <quanhthe181258@fpt.edu.vn>
2025-10-25 21:34:29 +0700

    Chỉnh sửa ConnectionString trong DbContext

--- NB.Model/Entities/NutriBarnContext.cs
+++ NB.Model/Entities/NutriBarnContext.cs
@@ -1,18 +1,23 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
 
 namespace NB.Model.Entities;
 
 public partial class NutriBarnContext : DbContext
 {
-    public NutriBarnContext()
+
+    private readonly IConfiguration _configuration;
+    public NutriBarnContext(IConfiguration configuration)
     {
+        _configuration = configuration;
     }
 
-    public NutriBarnContext(DbContextOptions<NutriBarnContext> options)
+    public NutriBarnContext(DbContextOptions<NutriBarnContext> options, IConfiguration configuration)
         : base(options)
     {
+        _configuration = configuration;
     }
 
     public virtual DbSet<Category> Categories { get; set; }
@@ -58,8 +63,13 @@ public partial class NutriBarnContext : DbContext
     public virtual DbSet<Worklog> Worklogs { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(local);Database=NutriBarn;User Id=sa;Password=123;TrustServerCertificate=true");
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
