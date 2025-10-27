493c1efdaa0a7733e4096956bc7b81d80224debc
Anhpqdhe172622 <anhpqdhe172622@fpt.edu.vn>
2025-10-27 20:41:12 +0700

    Update Program.cs

--- NB.API/Program.cs
+++ NB.API/Program.cs
@@ -1,19 +1,7 @@
 ﻿using Autofac;
 using Autofac.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using NB.API.Modules;
-using NB.Model.Entities;
-using NB.Repository.Common;
-using NB.Service.CategoryService;
 using NB.Service.Core.Mapper;
-using NB.Service.InventoryService;
-using NB.Service.ProductService;
-using NB.Service.UserService;
-using NB.Service.RoleService;
-using NB.Service.UserRoleService;
-using NB.Service.SupplierService;
-using NB.Service.WarehouseService;
 
 var builder = WebApplication.CreateBuilder(args);
 
