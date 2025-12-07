66c23dc747d5b74b81baa0220e4765bbe8014fb3
QuanHTHE181258 <quanhthe181258@fpt.edu.vn>
2025-12-07 23:19:47 +0700

    Sửa lỗi property uppercase

--- NB.Services/UserService/ViewModels/CreateCustomerAccountVM.cs
+++ NB.Services/UserService/ViewModels/CreateCustomerAccountVM.cs
@@ -7,22 +7,22 @@ namespace NB.Service.UserService.ViewModels
     {
         [Required(ErrorMessage = "Username là bắt buộc")]
         [StringLength(50, ErrorMessage = "Username không được vượt quá 50 ký tự")]
-        public string Username { get; set; } = null!;
+        public string username { get; set; } = null!;
 
         [Required(ErrorMessage = "Tên người dùng là bắt buộc")]
         [StringLength(100, ErrorMessage = "Tên người dùng không được vượt quá 100 ký tự")]
-        public string FullName { get; set; } = null;
+        public string fullName { get; set; } = null;
 
         [Required(ErrorMessage = "Email là bắt buộc")]
         [EmailAddress(ErrorMessage = "Email không hợp lệ")]
         [StringLength(100, ErrorMessage = "Email không được vượt quá 100 ký tự")]
-        public string Email { get; set; } = null!;
+        public string email { get; set; } = null!;
 
         [Required(ErrorMessage = "Số điện thoại là bắt buộc")]
         [Phone(ErrorMessage = "Số điện thoại không hợp lệ")]
         [StringLength(20, ErrorMessage = "Số điện thoại không được vượt quá 20 ký tự")]
-        public string Phone { get; set; } = null!;
+        public string phone { get; set; } = null!;
 
-        public IFormFile? Image { get; set; }
+        public IFormFile? image { get; set; }
         }
 }
